7ab8155e64b33ef2e3ee1e31056631e208417712
BrunoPanassi <bhenrique95@outlook.com>
2023-12-06 07:41:31 -0300

    add text input and dataUrl

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,46 @@
 <template>
     <div>
         <p>Text to Carousel</p>
+        <input type="text" v-model="text">
+        <div>
+            <img :src="imageUrl">
+        </div>
     </div>
 </template>
 
 <script setup>
+import { UltimateTextToImage} from "ultimate-text-to-image";
+import { ref, watch } from "vue";
+
+let text = ref("");
+
+const options = {
+    width: 1000,
+    height: 1000,
+    maxWidth: 1000,
+    maxHeight: 1000,
+    fontFamily: "Arial",
+    fontColor: "#FFFFFF",
+    fontSize: 72,
+    minFontSize: 10,
+    lineHeight: 50,
+    margin: 20,
+    marginBottom: 40,
+    align: "center",
+    valign: "middle",
+    borderColor: 0xFF000099,
+    borderSize: 2,
+    backgroundColor: "#000000",
+    underlineSize: 2,
+};
+
+let textToImage = ref("");
+
+const imageUrl = computed(() => textToImage.value)
+
+watch(text, () => {
+    textToImage.value = new UltimateTextToImage(text.value, options).render().toDataUrl();
+})
 
 </script>
 
