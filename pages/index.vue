784bc905118b2ceb4ad9f3e72999baf68e242e57
BrunoPanassi <bhenrique95@outlook.com>
2023-12-07 13:15:58 -0300

    create carousel and dynamic input texts

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,37 @@
 <template>
     <v-app>
         <v-container>
-            <v-text-field label="Primeira Imagem" v-model="text">
-            </v-text-field>
-            <v-btn>Adicionar</v-btn>
-            <div>
-                <img alt="" :src="imageUrl" />
-            </div>
+            <v-row v-for="(input, i) in inputs">
+                <v-text-field v-model="input.text" :key="i"></v-text-field>
+            </v-row>
+            <v-btn @click="addInput">Adicionar</v-btn>
+            <v-carousel>
+                <v-carousel-item v-for="(image, key) in images" :key="key" :src="image">
+                </v-carousel-item>
+            </v-carousel>
         </v-container>
     </v-app>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import TextToImage from "@/service/textToImage";
 import { ref } from "vue";
 
-let text = ref("");
+interface Text {
+    text: string
+}
 
-const imageUrl = computed(() => TextToImage.render(text.value))
+let inputs: Ref<Array<Text>> = ref([{text: ""}]);
+function addInput() {
+    inputs.value.push({text: ""})
+}
+
+function removeInput(index: number) {
+    inputs.value.splice(index, 1)
+}
+
+let images = computed(() => inputs.value.map((i) => TextToImage.render(i.text)))
+
 
 </script>
 
