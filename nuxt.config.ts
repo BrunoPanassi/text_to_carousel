c40d949717746b147f393ce5ce4c22492858ae64
BrunoPanassi <bhenrique95@outlook.com>
2024-01-09 07:24:09 -0300

    declare process env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { loadEnv } from "vite"
+const env = loadEnv(import.meta.env.MODE, process.cwd(), "")
 export default defineNuxtConfig({
   devtools: { enabled: true },
   build: {
@@ -16,6 +18,9 @@ export default defineNuxtConfig({
     //...
   ],
   vite: {
+    define: {
+      "process.env": env
+    },
     vue: {
       template: {
         transformAssetUrls,
